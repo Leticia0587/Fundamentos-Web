36f8c03b554f68c90dd0a75ab64869883e04f238
unknown <leticiadearaujo005@gmail.com>
2022-09-26 16:15:15 -0300

    pag login 2

---
+++ Formulario/assets/js/script.js
@@ -0,0 +1,9 @@
+/*
+Case Sensitive = reconhece letras maiusculas e minusculas 
+
+ por Tag: getElementByTagName()
+ por Id: getElementById()
+ por Nome: getElementsByName()
+ por Classe: getElementsByClassName()
+ por Seletor: querySelector()
+ */
